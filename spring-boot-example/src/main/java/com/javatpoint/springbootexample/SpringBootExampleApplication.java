4198fcb12314876cdf20818e4fe3de1e9040c19c
bhava <bhava@DESKTOP-A3E40Q2>
2022-11-18 22:50:46 +0530

    Updating The Code

--- spring-boot-example/src/main/java/com/javatpoint/springbootexample/SpringBootExampleApplication.java
+++ spring-boot-example/src/main/java/com/javatpoint/springbootexample/SpringBootExampleApplication.java
@@ -1,18 +1,18 @@
 package com.javatpoint.springbootexample;
-<<<<<<< HEAD
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-
-public class SpringBootExampleApplication 
-{
-public static void main(String[] args) 
-{
-SpringApplication.run(SpringBootExampleApplication.class, args);
-System.out.println("Hello World !!!!");
-}
-}
-=======
+//<<<<<<< HEAD
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@SpringBootApplication
+//
+//public class SpringBootExampleApplication 
+//{
+//public static void main(String[] args) 
+//{
+//SpringApplication.run(SpringBootExampleApplication.class, args);
+//System.out.println("Hello World !!!!");
+//}
+//}
+//=======
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,4 +26,4 @@ public class SpringBootExampleApplication
     System.out.println("***Hello World !!!!***");
   }
 }
->>>>>>> branch 'main' of https://github.com/SanketBhavar/SpringBootProject.git
+//>>>>>>> branch 'main' of https://github.com/SanketBhavar/SpringBootProject.git
